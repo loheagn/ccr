359f583f8cad941b6d4e372bc38480ca3781a7aa
loheagn <loheagn@icloud.com>
2024-03-31 12:42:39 +0000

    fix overlaynfs background copy

--- overlaynfs/mount_test.go
+++ overlaynfs/mount_test.go
@@ -3,9 +3,11 @@ package overlaynfs
 import "testing"
 
 func Test_mount(t *testing.T) {
-	sbID := "container"
-	containerID := "container"
+	sbID := "cp-mysql-nfs-id-3"
+	containerID := "container-test"
 	_, err := Mount(sbID, containerID)
+	done := make(chan bool)
+	<- done
 	if err != nil {
 		t.Fatal(err)
 	}
